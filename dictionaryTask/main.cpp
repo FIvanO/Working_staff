11ec074405507d0c144109837e15761696b99090
FIvanO <FIvanO1998@gmail.com>
2018-05-17 17:04:11 +0300

    refactoring

--- dictionaryTask/main.cpp
+++ dictionaryTask/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdio>
 #include <map>
 
@@ -9,21 +10,29 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::map;
+using std::ifstream;
 
-int letterW(char l) { // calculate weight for every letter
+// set ifstream for input file or throwing exception, if file name incorrect or doesn't exist
+void setFileStream(const string &inputFile, ifstream &fIn) {
+	string input = "../" + inputFile;
+	fIn = ifstream(input);
+	if (!fIn.is_open()) {
+		throw ("File name incorrect");
+	}
+}
+
+// calculate weight for every letter
+int letterW(char l) {
 	return int(l - 'A') + 1;
 }
 
-LL calculateDictionary(const string &inputFile) {
-	string input = "../" + inputFile;
-	freopen(input.c_str(), "r", stdin); // for reading our input string
-	map<string, int> weidth; // weidth for every word
-
+// fulling array with weidth for every word
+void calculateWordWeidth(map<string, int> &weidth, ifstream &fInput) {
 	char c;
 	bool start = false; // should be true, when we start reading the next word, else false
 	string word = ""; // for making word from input string
 	int weidth_ = 0; // for calculating weidth for each word
-	while (cin >> c) {
+	while (fInput >> c) {
 		if (c == '\"') { // if we read a " we changing our state (variable start)
 			if (!start) { // it will be a new word
 				start = true;
@@ -42,20 +51,38 @@ LL calculateDictionary(const string &inputFile) {
 			weidth_ += letterW(c); // calculating weidth of word
 		}
 	}
+}
 
+// calcuate final weidth taking into consideration the position of each word in alphabetical order
+LL getFinalWeidth(map<string, int> &weidth) {
 	int k = 1; // position in dictionary
 	LL ans = 0; // sum of all weidth
 	for (auto i = weidth.begin(); i != weidth.end(); ++i) {
-		cout << i->first << ": " << i->second * k << endl;
 		ans += i->second * k;
 		k++;
 	}
 	return ans;
 }
 
+// control function
+LL calculateDictionary(const string &inputFile) {
+	ifstream fInput;
+	try {
+		setFileStream(inputFile, fInput);
+	}
+	catch (const char *ex) {
+		cout << ex << endl;
+		return -1;
+	}
+	map<string, int> weidth; // weidth for every word
+	calculateWordWeidth(weidth, fInput);
+	fInput.close();
+	return getFinalWeidth(weidth);
+}
+
 int main()
 {
-	string inputFile = "Input.txt"; // input file name, should be in the folder with project folder
+	string inputFile = "names.txt"; // input file name, should be in the folder with project folder
 	cout << calculateDictionary(inputFile) << endl;
 	return 0;
 }
